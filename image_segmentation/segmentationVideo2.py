be1d66101f01c7b7d0f54b08d70b2ee8797c6289
Rubey4112 <74166553+Rubey4112@users.noreply.github.com>
2024-10-12 12:39:34 -0400

    updated segmentationVideo2

---
+++ image_segmentation/segmentationVideo2.py
@@ -0,0 +1,37 @@
+import cv2, time
+from ultralytics import YOLO
+
+model = YOLO("yolov8m-seg.pt")
+# model = FastSAM("FastSAM-s.pt")
+
+video = "SegmentationTestVid.mov"
+results = model(video, stream = True, show = True)
+
+for r in results:
+    print(r.masks)  # print the Masks object containing the detected instance masks
+# cap = cv2.VideoCapture(video)
+
+# while cap.isOpened():
+
+#     success, frame = cap.read()
+
+#     if success:
+#         start = time.perf_counter()
+#         results = model(frame, save = True)
+
+#         end = time.perf_counter()
+#         total_time = end - start
+#         fps = 1 / total_time
+
+#         annotated_frame = results[0].plot()
+
+#         cv2.putText(annotated_frame, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+#         cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+#         if cv2.waitKey(1) & 0xFF == ord("q"):
+#             break
+#     else:
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
